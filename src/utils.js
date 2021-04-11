b88612d2cdfd2d8c318b90db1fe2eb3d018774f8
J-K. Solbakken <jksolbakken@gmail.com>
2021-04-11 12:45:25 +0200

    better naming

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
-const csvToArray = (str) => str ? str.split(',').map((feature) => feature.trim())  : []
+const csvToArray = (str) => str ? str.split(',').map((element) => element.trim())  : []
 
 const decodeB64 = (b64Txt) => Buffer.from(b64Txt, 'base64').toString()
 
